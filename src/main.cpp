5ae096247f1a24cbf84991545a1490e2ab5a75b5
Alexandr Tatianin <tan_stc@mail.ru>
2019-07-23 19:09:58 +0300

    Осталось немного

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,45 @@
 #include <iostream>
+#include <assert.h>
 #include "version.h"
 #include "matrix.h"
-int main(int argc, char const *argv[])
+
+void test()
 {
     Matrix<int, -1> matrix; // бесконечная матрица int заполнена значениями -1
-    //assert(matrix.size() == 0); // все ячейки свободны
-    //auto a = matrix[0][0];
-    //assert(a == -1);
-    //assert(matrix.size() == 0);
+    assert(matrix.size() == 0); // все ячейки свободны
+    auto a = matrix[0][0];
+    assert(a == -1);
+    assert(matrix.size() == 0);
     matrix[100][100] = 314;
-    //assert(matrix[100][100] == 314);
-    //assert(matrix.size() == 1);
+    assert(matrix[100][100] == 314);
+    assert(matrix.size() == 1);
+    for (auto c : matrix)
+    {
+        int x;
+        int y;
+        int v;
+        std::tie(x, y, v) = c;
+        std::cout << x << y << v << std::endl;
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+    test();
+    Matrix<int, 0> matrix;
+    for (int i = 0; i < 10; i++)
+    {
+        matrix[i][i] = i;
+        matrix[9 - i][i] = 9 - i;
+    }
+
+    for (int x = 1; x <= 8; x++)
+    {
+        for (int y = 1; y <= 8; y++)
+            std::cout << (int)matrix[x][y] << ' ';
+        std::cout << std::endl;
+    }
+
+    std::cout << matrix.size();
     return 0;
 }
--- src/matrix.h
+++ src/matrix.h
@@ -7,53 +7,94 @@ class Matrix
 {
 private:
     struct Cell;
+    struct Column;
     using column_t = std::map<uint64_t, Cell>;
-    using column_ptr_t = column_t*;
+    using matrix_t = std::map<uint64_t, Column>;
 
     struct Cell
     {
         uint64_t x_;
         uint64_t y_;
         T value_;
-        column_ptr_t p_column_;
-
-        Cell(uint64_t x, uint64_t y):x_(x), y_(y), value_(ZERO_VAL){};
+        uint64_t *matrix_size_;
+        Cell(uint64_t x, uint64_t y, uint64_t *matrix_size):x_(x), y_(y), value_(ZERO_VAL), matrix_size_(matrix_size){};
 
         Cell& operator=(const T& value)
         {
+            if (value_ == ZERO_VAL && value != ZERO_VAL)
+                (*matrix_size_)++;
+            else if(value_ != ZERO_VAL && value == ZERO_VAL)
+                (*matrix_size_)--;
             value_ = value;
-            if(value_ == ZERO_VAL)
-            {
-                auto p_column = p_column_;
-                p_column->erase(y_);
-                if(p_column->empty())
-                    p_column->p_matrix_->erase(x_);
-            }
+            return *this;
         }
+        template<class U>
+        operator std::tuple<U&, U&, T&>()
+        {
+            return std::tie((U&)x_, (U&)y_, value_);
+        }
+        template<class U>
+        operator U()
+        {
+            return static_cast<U>(value_);
+        }
+        bool operator== (const T& value) const { return value_ == value; };
     };
-    struct Column;
-    using matrix_t = std::map<uint64_t, Column>;
-    using matrix_ptr_t = matrix_t*;
-    
+
     struct Column
     {
         const uint64_t x_;
-        matrix_ptr_t p_matrix_;
         column_t column_;
-        Cell& operator[](const uint64_t y)
+        uint64_t *matrix_size_;
+        Cell& operator[](uint64_t y)
         {
-            return *(column_.emplace({y, Cell(x_,y)}).first);
+            return column_.insert(std::make_pair(y, Cell(x_,y, matrix_size_))).first->second;
         }
-        Column(uint64_t x, matrix_ptr_t p_matrix):x_(x),p_matrix_(p_matrix){};
+        Column(uint64_t x, uint64_t *matrix_size):x_(x), matrix_size_(matrix_size){};
+        typename column_t::iterator begin() { return column_.begin(); }
+        typename column_t::iterator end() { return column_.end(); }
     };
 
     matrix_t matrix_; 
+    uint64_t size_;
 public:
-    Matrix(/* args */){};
+    Matrix():size_(0){};
     ~Matrix(){};
     Column& operator[](uint64_t x)
     {
-        return matrix_.insert(std::make_pair(x, Column(x, &matrix_)).first.second;
-    }
+        return matrix_.insert(std::make_pair(x, Column(x, &size_))).first->second;
+    };
+    uint64_t size() { return size_; };
+
+    class matrix_iterator
+    {
+        typename matrix_t::iterator mit_;
+        typename column_t::iterator cit_;
+    public:
+        matrix_iterator() = default;
+        matrix_iterator(typename matrix_t::iterator mit) :mit_(mit){};
+        matrix_iterator(const matrix_iterator &rhs) :mit_(rhs.mit_), cit_(rhs.cit_){};
+        const matrix_iterator& operator=(const matrix_iterator& rhs) { mit_ = rhs.mit_; return *this; }
+
+        std::tuple<int&, int&,T&> operator*() { return  (std::tuple<int&, int&, T&>)cit_->second;}
+        Cell* operator->() { return cit->second; }
+        const bool operator!=(const matrix_iterator& rhs)const { return !(mit_ == rhs.mit_ && cit_ == rhs.cit_); }
+        matrix_iterator& operator++()
+        {
+            if (cit_ == mit_->second.end())
+            {
+                mit_++;
+                cit_ = mit_->second.begin();
+            }
+            else
+                cit_++;
+            return *this; 
+        };
+    };
+
+    using iterator = matrix_iterator;
+    iterator begin() { return iterator(matrix_.begin()); }
+    iterator end() { return iterator(matrix_.end()); }
+
 };
 
